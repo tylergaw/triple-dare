c0bae4dc83681dcf4cc1308b4489bd8fb40800a6
Tyler Gaw <me@tylergaw.com>
2011-10-20 23:09:18 -0400

    Adding CSS dir

---
+++ css/config.rb
@@ -0,0 +1,13 @@
+# This is a compass (sass/stylesheet) configuration file.
+#
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "scss"
+images_dir = "../images"
+javascripts_dir = "../js"
+output_style = :compressed
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+preferred_syntax = :scss
