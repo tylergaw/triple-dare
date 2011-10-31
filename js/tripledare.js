0f52d3b9a7303ea034949af921da681e80eb31a7
Tyler Gaw <me@tylergaw.com>
2011-10-30 23:15:20 -0400

    Stuff

--- js/tripledare.js
+++ js/tripledare.js
@@ -1,111 +1,111 @@
 (function () {
-	
+
 	var 
 		// Ran into a number of edge-cases with styling that I
 		// can't seem to get around with feature detection, so
 		// falling back to some good ole browser sniffing to 
 		// handle the few outliers.
 		featureAdditionsBySniffing = function () {
-		
+			
 			// A modified version of Quirksmode's BrowserDetect
 			// http://www.quirksmode.org/js/detect.html
 			var browserDetect = {
-				init: function () {
-					this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
-					this.OS = this.searchString(this.dataOS) || "an unknown OS";
-				},
-				searchString: function (data) {
-					var i          = 0,
-						len        = data.length,
-						dataString = null,
-						dataProp   = null,
-						identity   = null;
-        	
-					for (i; i < len; i += 1) {
-						dataString = data[i].string;
-						dataProp   = data[i].prop;
-        	
-						if (dataString) {
-							if (dataString.indexOf(data[i].subString) != -1) {
+					init: function () {
+						this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+						this.OS = this.searchString(this.dataOS) || "an unknown OS";
+					},
+					searchString: function (data) {
+						var i          = 0,
+							len        = data.length,
+							dataString = null,
+							dataProp   = null,
+							identity   = null;
+        	    	
+						for (i; i < len; i += 1) {
+							dataString = data[i].string;
+							dataProp   = data[i].prop;
+        	    	
+							if (dataString) {
+								if (dataString.indexOf(data[i].subString) != -1) {
+									identity = data[i].identity;
+									break;
+								}
+							} else if (dataProp) {
 								identity = data[i].identity;
 								break;
 							}
-						} else if (dataProp) {
-							identity = data[i].identity;
-							break;
+						}
+        	    	
+						return identity;
+					},
+					dataBrowser: [
+						{
+							string: navigator.userAgent,
+							subString: "Chrome",
+							identity: "Chrome"
+						},
+						{
+							string: navigator.vendor,
+							subString: "Apple",
+							identity: "Safari",
+							versionSearch: "Version"
+						},
+						{
+							prop: window.opera,
+							identity: "Opera",
+							versionSearch: "Version"
+						},
+						{
+							string: navigator.userAgent,
+							subString: "Firefox",
+							identity: "Firefox"
+						},
+						{
+							string: navigator.userAgent,
+							subString: "MSIE",
+							identity: "Explorer",
+							versionSearch: "MSIE"
+						}
+					],
+					dataOS : [
+						{
+							string: navigator.platform,
+							subString: "Win",
+							identity: "Windows"
+						},
+						{
+							string: navigator.platform,
+							subString: "Mac",
+							identity: "Mac"
+						},
+						{
+							string: navigator.userAgent,
+							subString: "iPhone",
+							identity: "iPhone/iPod"
+					    },
+						{
+							string: navigator.userAgent,
+							subString: "iPad",
+							identity: "iPad"
+					    }
+					]
+				},
+			
+				// Safari (as of 5.1) has a bug where @font-face fonts
+				// cannot be used with select elements. Using them together
+				// causing the selects to not work, and makes the browser
+				// go totally apeshit. iOS Safari does not have this problem.
+				fontFaceSelects = function () {
+					var b = true;
+        		
+					if (browserDetect.browser === 'Safari') {
+						if (browserDetect.OS.indexOf('iP') === -1) {
+							b = false;
 						}
 					}
-        	
-					return identity;
-				},
-				dataBrowser: [
-					{
-						string: navigator.userAgent,
-						subString: "Chrome",
-						identity: "Chrome"
-					},
-					{
-						string: navigator.vendor,
-						subString: "Apple",
-						identity: "Safari",
-						versionSearch: "Version"
-					},
-					{
-						prop: window.opera,
-						identity: "Opera",
-						versionSearch: "Version"
-					},
-					{
-						string: navigator.userAgent,
-						subString: "Firefox",
-						identity: "Firefox"
-					},
-					{
-						string: navigator.userAgent,
-						subString: "MSIE",
-						identity: "Explorer",
-						versionSearch: "MSIE"
-					}
-				],
-				dataOS : [
-					{
-						string: navigator.platform,
-						subString: "Win",
-						identity: "Windows"
-					},
-					{
-						string: navigator.platform,
-						subString: "Mac",
-						identity: "Mac"
-					},
-					{
-						string: navigator.userAgent,
-						subString: "iPhone",
-						identity: "iPhone/iPod"
-				    },
-					{
-						string: navigator.userAgent,
-						subString: "iPad",
-						identity: "iPad"
-				    }
-				]
-			},
-			
-			// Safari (as of 5.1) has a bug where @font-face fonts
-			// cannot be used with select elements. Using them together
-			// causing the selects to not work, and makes the browser
-			// go totally apeshit. iOS Safari does not have this problem.
-			fontFaceSelects = function () {
-				var b = true;
-        	
-				if (browserDetect.browser === 'Safari') {
-					if (browserDetect.OS.indexOf('iP') === -1) {
-						b = false;
-					}
-				}
-        	
-				return b;
-			};
+        		
+					return b;
+				};
 			
 			// Start up that browserDetect object
 			browserDetect.init();
@@ -167,7 +167,7 @@
 					time -= 1;
 				},
 				alarm = function () {
-					var alarmDuration = 3500;			
+					var alarmDuration = 3500;	
 					el.className = outOfTimeClass;
 					
 					setTimeout(function () {
@@ -176,9 +176,17 @@
 				};
 			
 			interval = setInterval(tick, tickSpeed);
+		},
+		
+		// For browsers that do not yet support the placeholder attribute
+		// for input elements.
+		placeholderPolyfill = function () {
+			
 		};
 	
 	// Start these boss hogs up!
 	featureAdditionsBySniffing();
 	timer();
+	placeholderPolyfill();
+	
 }());
